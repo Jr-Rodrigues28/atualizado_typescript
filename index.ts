1b39702f85f717be86e4ccc943acb47a6e042557
Rodrigues <7jsdrcj@gmail.com>
2025-09-22 22:24:22 -0300

    teste

---
+++ index.ts
@@ -0,0 +1,16 @@
+const nome = "Junior";
+const sobrenome = "Rodrigues";
+const idade = 20;
+const enredeco = "Rua das Flores";
+
+function greetings(name: string) {
+	console.log(`Olá ${name}`);
+}
+greetings("Lucas");
+
+const obj = {
+	chave: 2,
+	nome: "lucas",
+	prop: 10,
+	idade: 41,
+};
