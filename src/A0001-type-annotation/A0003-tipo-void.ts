81aeff26c84de49102ee9bed97ea54a26d818361
Junior Rodrigues <7jsdrcj@gmail.com>
2025-09-24 10:47:37 -0300

    Tipo VOID

---
+++ src/A0001-type-annotation/A0003-tipo-void.ts
@@ -0,0 +1,22 @@
+// Quer dizer que uma função ou um metodo não retorna nada
+
+function semRetorno(...args: string[]): void {
+	console.log(args.join(" "));
+}
+
+const pessoa = {
+	nome: "Junior",
+	sobrenome: "Rodrigues",
+
+	// metodo é uma função que está dentro de um objeto ou de uma classe
+	exibirNome(): void {
+		console.log(this.nome + " " + this.sobrenome);
+	},
+};
+
+semRetorno("Junior", "Roddrigues");
+pessoa.exibirNome();
+
+// quando usa export estamos em module
+// vamos no packge.json e mudamos em type para: module
+export { pessoa };
