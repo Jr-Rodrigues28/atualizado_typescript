02b673f15a5699fe62fcc2c8c543a9af0cadf8b1
Rodrigues <7jsdrcj@gmail.com>
2025-10-05 01:18:40 -0300

    função

---
+++ src/A0001-type-annotation/funcao.ts
@@ -0,0 +1,12 @@
+function serhumano(nome: string, idade: number, status: boolean){
+    console.log(`Olá, meu nome é ${nome}, tenho ${idade} anos, solteiro? ${status ? "sim" : "não"}`);
+}
+
+serhumano("Junior", 41, false);
+
+
+function comida(prato: string, qtd: number, molho: boolean) {
+    console.log(`Nome do prato: ${prato}, pra quantas pessoas: ${qtd} pessoas, com ou sem molho: ${molho ? "Com" : "Sem"}`)
+}
+
+comida("Macarrão", 4, true);
