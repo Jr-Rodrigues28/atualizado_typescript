9e61d22d67970a8ffc532ed96f55840cf042e838
Rodrigues <7jsdrcj@gmail.com>
2025-10-05 02:01:47 -0300

    Atualizando Função

--- src/melda.ts
+++ src/melda.ts
@@ -2,3 +2,13 @@ function oi() {
 	const frase = "oi";
 	return frase;
 }
+
+
+
+function escola(professor: string) {
+	let prof: string = "Junior";
+	console.log(`${prof}, ${professor}`)
+	return professor;
+}
+
+escola("Rodrigues");
